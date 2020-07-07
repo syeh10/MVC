ab98c5132ca0fe294d45473e57013a5dfafab93a
syeh10 <42697776+syeh10@users.noreply.github.com>
2020-07-07 11:46:25 +1000

    Update movie_zone_member_config.php

--- member/movie_zone_member_config.php
+++ member/movie_zone_member_config.php
@@ -49,9 +49,9 @@ if (empty($_SESSION['authorised'])) {
 */
 /*This is a good practice to define all constants which may be used at different places*/
 // define ('DB_CONNECTION_STRING', "mysql:host=localhost;dbname=movie_zone_db");
-define ('DB_CONNECTION_STRING', "mysql:host=infotech.scu.edu.au;dbname=syeh10CSC10217Ass1");
-define ('DB_USER', "syeh10");
-define ('DB_PASS', "22862541");
+define ('DB_CONNECTION_STRING', "mysql:host=confidential;dbname=confidential");
+define ('DB_USER', "confidential");
+define ('DB_PASS', "confidential");
 define ('MSG_ERR_CONNECTION', "Open connection to the database first");
 //maximum number of random movies will be shown
 define ('MAX_RANDOM_MOVIES', 4);
